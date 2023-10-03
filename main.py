5d7ec56b121b3a69d6168a4109359eea5c4fcf4d
giammy <122522075+giammyboy@users.noreply.github.com>
2023-10-03 12:42:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import pendoli
+import pendolo
+import numpy
 from numpy import pi
+import time
 
 
 # istanziamento dei pendoli, i parametri passati sono posizione iniziale e velocità angolare iniziale (di default = 0)
-p1 = pendoli.Pendolo(thetaIniz=pi / 18)
-p2 = pendoli.Pendolo(thetaIniz=-pi / 3)
-p5 = pendoli.Pendolo(thetaIniz=pi / 5)
+p1 = pendolo.Pendolo(thetaIniz=pi / 18)
+p2 = pendolo.Pendolo(thetaIniz=-pi / 3)
+p5 = pendolo.Pendolo(thetaIniz=pi / 5)
 
 # istanziamento supporto, i parametri sono: pendoli da mettere sopra, M, m, l
-Sys = pendoli.Sistema(pend=[p1, p2, p5], Mass=5, mass=1, lenght=0.3)
+Sys = pendolo.Sistema(pend=[p1, p2, p5], Mass=5, mass=1, lenght=0.3)
 # funzione per simulare l'evoluzione, i parametri sono: durata, timestep, presenza del termine di damping-driving,
 # coefficiente miu, angolo di equilibrio
-Sys.simula(100, 0.01, damping=True, mi=0.6)
+ti = time.time()
+Sys.simula(10, 0.001, damping=0, mi=0.6, useSciPy = 1)
+tf = time.time()
+print("delta time = ", tf - ti)
+#Sys.grafici_pend()
 Sys.animazione()
 
 
